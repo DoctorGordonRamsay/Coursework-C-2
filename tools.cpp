8f2e71c1893800e92c78cabc8337cafcf6cbcb01
The Good Doctor <158500189+DoctorGordonRamsay@users.noreply.github.com>
2024-12-03 16:05:09 +0000

    Add files via upload

--- tools.cpp
+++ tools.cpp
@@ -106,17 +106,75 @@ void tools::selectCity(city listofcities[], city matchedCities[], uint8_t matche
 }
 
 void tools::CalculateDistance(city listofcities[], city matchedCities[],  uint8_t selection, uint8_t capacity){
-
+    //
     // int longitude1 = matchedCities[selection].location[0];
     // int latitude1 = matchedCities[selection].location[1];
     //
+    // //search second city
+    //
+    // string search;
+    // cout << "Search second city by name: ";
+    // cin >> search;
+    //
+    // uint8_t matchedCount = 0;
+    // bool found = false;
+    // city matchedCities2[10];
+    //
+    // if (found) {
+    //     cout << "Cities found:\n";
+    //     for (int i = 0; i < matchedCount; i++) {
+    //         cout << "{{"
+    //              << matchedCities2[i].details[0] << " "
+    //              << matchedCities2[i].details[1] << " "
+    //              << matchedCities2[i].details[2] << " "
+    //              << matchedCities2[i].details[3] << "}{"
+    //              << matchedCities2[i].location[0] << " "
+    //              << matchedCities2[i].location[1] << "}}\n";
+    //     }
     //
     //
+    // for (int i = 0; i < capacity; i++) {
+    //     if (listofcities[i].details[0] == search) {
+    //         if (matchedCount < 10) {
+    //             matchedCities2[matchedCount] = listofcities[i];
+    //             matchedCount++;
+    //         }
+    //         found = true;
+    //     } else {
+    //         cout << search << " was not found.";
+    //     }
+    // }
     //
+    //     //select city
     //
-    // uint32_t distance;
+    //     cout << "select a city:\n";
+    //     for (int i = 0; i < matchedCount; i++) {
+    //         cout << i;
+    //         cout << ":  "
+    //              << matchedCities2[i].details[0] << "  "
+    //              << matchedCities2[i].details[1] << "  "
+    //              << matchedCities2[i].details[2] << "  "
+    //              << matchedCities2[i].details[3] << "  "
+    //              << matchedCities2[i].location[0] << "  "
+    //              << matchedCities2[i].location[1] << "  \n";
+    //     }
     //
-    // cos(distance) = sin(latitude1) * sin(latitude2) + cos(latitude1) * cos(latitude2) *cos(longitude1 - Longitude2);
+    //     int selection2;
+    //     cout << "Enter the number: ";
+    //     cin >> selection2;
+    //
+    //     if (selection2 >= 0 && selection2 < matchedCount) {
+    //         cout << "You selected: " << matchedCities2[selection2].details[0] << " - " << matchedCities2[selection2].details[1] << " - " << matchedCities2[selection2].details[2] << endl;
+    //     } else {
+    //         cout << "Invalid option.\n";
+    //     }
+    //
+    //     int longitude2 = matchedCities2[selection2].location[0];
+    //     int latitude2 = matchedCities2[selection2].location[1];
+    //
+    // int distance;
+    //
+    // cos(distance) = sin(latitude1) * sin(latitude2) + cos(latitude1) * cos(latitude2) *cos(longitude1 - longitude2);
     //
     // distance = (6371 * M_PI * distance) / 180;
     //
